dfb24b93a532e0d91da0b6ba456aaa80a7449ab5
sanil sarathe <sanilsarathe76@gmail.com>
2024-02-27 20:01:40 +0530

    Card UI

---
+++ Hike/Components/CustomBackgroundView.swift
@@ -0,0 +1,39 @@
+//
+//  CustomBackgroundView.swift
+//  Hike
+//
+//  Created by macmini41 on 27/02/24.
+//
+
+import SwiftUI
+
+struct CustomBackgroundView: View {
+    var body: some View {
+        ZStack {
+            Color.customGreenDark
+                .cornerRadius(40)
+                .offset(y: 12)
+            
+            Color.customGreenLight
+                .cornerRadius(40)
+                .offset(y: 3)
+                .opacity(0.85)
+            
+            LinearGradient(
+                colors: [
+                    .customGreenDark,
+                    .customGreenMedium],
+                startPoint: .top,
+                endPoint: .bottom
+            )
+            .cornerRadius(40)
+        }
+    }
+}
+
+struct CustomBackgroundView_Previews: PreviewProvider {
+    static var previews: some View {
+        CustomBackgroundView()
+            .padding()
+    }
+}
---
+++ Hike/HikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikeApp.swift
+//  Hike
+//
+//  Created by macmini41 on 27/02/24.
+//
+
+import SwiftUI
+
+@main
+struct HikeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
